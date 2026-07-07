e8dd0a9f069f2d834a874bd8a9b9479885ad2712
Daniel Rösch <droesch@data-al.local>
2026-07-07 17:00:10 +0200

    adjust level selection screen to work properly and have a return button that takes you back to the main menu, add and update todos

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import sys
 from game import Game
 
 # DONE TODO: BUG: Sticky paddle needs to allow for angled shots when the ball sticks to it non-centered.
+# TODO: FEATURE: Add a scoring system that calculates a score based on the amount of time that is needed to pass the level. Should also account for the amount and types of extras that you collect. Score should be visible at all times and should get shown again when a level has been completed.
+# TODO: FEATURE: Add a highscore system so that players can record their best runs.
+# TODO: FEATURE: Add a difficulty setting that will influence game speed, block health and power up distribution.
+# TODO: FEATURE: The level number should be clearly visible when playing any given level. This could get handled with the title of the window where the game is running, or it could display a text in the background of the level.
 # TODO: FEATURE: Add a screen that tells the player when a level has been completed. Should contain the current level number, (score?), and buttons to get back to the main menu, the editor or play the next level.
 # TODO: FEATURE: The main menu should have a link that opens the editor.
 # TODO: BUG: Regular bricks shouldn't spawn power ups as often.
 # TODO: FEATURE: Add more power ups.
+# TODO: FEATURE: Add a secure border power up. This will create a temporary line beneath our paddle and will deflect any ball that we might miss with our paddle back into the game area.
+# TODO: FEATURE: Add a magnet power up. Should attract the ball very slightly to move towards our paddle when already in a downwards decent. Should also affect the power ups that have been spawned and attract them to the paddle, as well. The attraction towards the power ups should be greater than the attraction to the ball, to make it harder for the player to avoid a negative effect. Maybe the ball magnet and the power up magnet should be two different power ups? Think about which way of implementing this would be the most beneficial.
 # TODO: FEATURE: Add more block types.
 # TODO: BUG: Sticky paddle needs to have a stricter timer.
 # TODO: FEATURE: Multiple effects currently already stack together, but multiple power ups of the same type could maybe increase the level of the effect, depending on the type. (e.g. wider paddle gives an even wider paddle when collected twice. but slower ball shouldn't bring the game speed down to unplayable levels.)
